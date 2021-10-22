61d6ab4cc856bc45f76f9953b8d844bc712e54ec
MyrddinoptRoodt <r0846529@student.thomasmore.be>
2021-10-22 10:28:52 +0200

    basic led function + reload button + bullet systeem

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,15 @@
 #include <assert.h>
 #include <FastLED.h>
 
-#define ledPin     13
+#define ledPin     13 //D7
 #define COLOR_ORDER RGB
 #define CHIPSET     WS2811
 #define NUM_LEDS    1
 
-#define BRIGHTNESS  200
-#define FRAMES_PER_SECOND 60
 #define KORT 393
 #define LANG 786 // gemiddelde van veel dingen
-const uint32_t kBaudRate = 115200;
-const uint16_t kCaptureBufferSize = 1024;
-CRGB leds[NUM_LEDS];
+const uint32_t kBaudRate = 115200; //bitrate
+const uint16_t kCaptureBufferSize = 1024; 
 
 const uint8_t kTimeout = 50;
 const uint16_t kMinUnknownSize = 12;
@@ -27,9 +24,9 @@ const uint16_t kMinUnknownSize = 12;
 const uint8_t kTolerancePercentage = kTolerance;  // kTolerance is normally 25%
 
 const uint16_t kIrLed = 4;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
-const uint16_t kRecvPin = 14;
+const uint16_t kRecvPin = 14; // pin to receive ir data
 
-
+CRGB leds[NUM_LEDS];
 IRrecv irrecv(kRecvPin, kCaptureBufferSize, kTimeout, true);
 decode_results results;  // Somewhere to store the results
 
@@ -38,8 +35,7 @@ IRsend irsend(kIrLed);  // Set the GPIO to be used to sending the message.
 // Example of data captured by IRrecvDumpV2.ino
 uint16_t rawData[] = { 1646 , 422 , 393, 393, 393, 393, 393, 393, 393, 786, 393, 786, 393, 786, 393, 786, 393, 1  , 2   ,3    ,4    ,5    ,6    ,7    ,8    ,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24, 5600};
 //uint16_t rawData[] =   { 1646 , 422 , 393, 393, 393, 393, 393, 393, 393, 786, 393, 786, 393, 786, 393, 786, 393, 393, 393 ,393  ,393  ,393  ,393  ,393  ,786  ,393, 393 ,393  ,393  ,393  ,393  ,393  ,786  ,393, 393 ,393  ,393  ,393  ,786 ,786  ,393  , 5600};
-// Example Samsung A/C state captured from IRrecvDumpV2.ino
-// Example Samsung A/C state captured from IRrecvDumpV2.ino
+// binary codes of the 'legacy guns', ffa gun is not legacy
 String BlueGunx1    = "00000001 00000001 00000110";
 String BlueGunx2    = "00000001 00000010 00000111";
 String BlueGunx3    = "00000001 00000011 00001000";
@@ -53,33 +49,41 @@ String WhiteGunx1   = "00000100 00000001 00001001";
 String WhiteGunx2   = "00000100 00000010 00001011";
 String WhiteGunx3   = "00000100 00000011 00001100";
 String FFAGUN       = "00000101 00000001 00001100";
-
+//codes for the teams
 String Blue   =   "001";
 String Red    =   "010";
 String Green  =   "011";
 String White  =   "100";
 String FFA    =   "101";
 String eigenteam = "101";
+int bullet1x = 6;
+int bullet2x = 3;
+int bullet3x = 1;
+
+
 int teams = 0;
 String gun = FFAGUN;
 int guns = 100;
-
+//codes for the damage
 String Damagex1   = "01";
 String Damagex2   = "10";
 String Damagex3   = "11";
 int buzzer = 15;  //  pin D8
 int ChangeTeams_Button = 12; //d6
-int button_Shoot = 5;    // pushbutton connected to digital pin D0
+int button_Shoot = 5;    // pushbutton connected to digital pin D1
 int val = 0;      // variable to store the read value
-int ChangeGuns_Button = 16;//d0
-
-int Health = 9;
-
+int ChangeGuns_Button = 16;//d0 button to change the gun type
+int Reload_Button = 0;//reload button on D3 (gpio 0)
+int bullets = 6;//amount of bullets for default gun
+int Health = 9; //you start with 9hp
+int bullet_type = bullet1x;
 
 void setup() {
   pinMode(buzzer, OUTPUT);  // sets the digital pin 13 as output
   pinMode(button_Shoot, INPUT);    // sets the digital pin 7 as input
   pinMode(ChangeGuns_Button,INPUT);
+  pinMode(Reload_Button,INPUT);
+  pinMode(ChangeTeams_Button, INPUT);
   irsend.begin();
   Serial.begin(115200, SERIAL_8N1, SERIAL_TX_ONLY);
   assert(irutils::lowLevelSanityCheck() == 0);
@@ -105,10 +109,10 @@ void buzzerfun(int repeat){
     delay(5);
     digitalWrite(buzzer, HIGH);
     i++;
-    
   }
   
 }
+
 String ChangeTeams(int teams){
   teams ++;
   if (teams == 5){
@@ -163,7 +167,7 @@ String ChangeTeams(int teams){
 
 }
 
-void prepare_shot(String shot){
+void prepare_shot(String shot){ //this function prepares, and fires the shot, first it will create a string to send in binary, then it will translate the string to a raw array, wich will be send as is
   String list = "01";
   int(i) = 0;
   int(array_place) = 17;
@@ -195,7 +199,7 @@ void prepare_shot(String shot){
 
 
 
-void get_damage(String temp){
+void get_damage(String temp){ //as the name sugests, this function stands in to calculate the received damage, it also calls the buzzer funcion for audibel feedback when shot
   if (temp == Damagex1)
   {
     Health --;
@@ -220,14 +224,14 @@ void get_damage(String temp){
   Serial.println(temp);
 }
 
-void decodeData(uint16_t * Datass){
+void decodeData(uint16_t * Datass){ //this function is to "decode" the data, since it is encoded "raw"
   String binairy_code = "";
   int(i) = 17;
   int(iteration) = 1;
-  while (i<41)
+  while (i<41) //this part decodes the received values to binary, the first values of the array is static, and already present in this code, the part between place 17 and 41 (not inclusive, also we are programmers: indexes start at 0)
   {
    String bin_old = binairy_code;
-   uint16_t value = Datass[i];
+   uint16_t value = Datass[i]; 
    if (value > 600)
    {
      binairy_code = bin_old + "1";
@@ -243,16 +247,16 @@ void decodeData(uint16_t * Datass){
    i++;
   }
   Serial.println("code: " + binairy_code);
-  Serial.println("code damage: " + binairy_code.substring(15,17));
+  Serial.println("code damage: " + binairy_code.substring(15,17)); //this gets a substring which contain the 'damage' digets to be printed, this will be seen again in the next short while
   bool b = true; 
   while (b)
   {
     
-    String temp = binairy_code.substring(5,8);
-    String damage = binairy_code.substring(15,17);
+    String temp = binairy_code.substring(5,8); //this gets the team value from the string
+    String damage = binairy_code.substring(15,17);//this (once again) will remember the damage value
     Serial.println(damage);
     Serial.println(temp);
-    if (((temp == Blue) or (temp == Red) or (temp == Green) or (temp == White)) and (temp != eigenteam))
+    if (((temp == Blue) or (temp == Red) or (temp == Green) or (temp == White)) and ((temp != eigenteam) and (temp != FFA))) 
     // if (((temp == Blue) or (temp == Red) or (temp == Green) or (temp == White)) and (temp != eigenteam) and (eigenteam != FFA)) 
     {
       get_damage(binairy_code.substring(15,17));
@@ -269,32 +273,43 @@ void decodeData(uint16_t * Datass){
   }
   
 }
-void changeGuns (){
+
+void changeGuns (){ //function that changes the gun type, it uses the switch method
   guns ++;
-  switch (teams)
+  switch (teams) // guns are 'different' for each team, so it first checks what team the gun currently is part of.
   {
   case 0:
     gun.clear();
     gun = FFAGUN;
+    bullets = bullet1x;
+    bullet_type = bullet1x;
     break;
   case 1:
     switch (guns)
     {
     case 11:
       gun = BlueGunx2;
+      bullets = bullet2x;
+      bullet_type = bullet2x;
       break;
     case 12:
       gun = BlueGunx3;
+      bullets = bullet3x;
+      bullet_type = bullet3x;
       break;
     case 13:
       gun = BlueGunx1;
+      bullets = bullet1x;
       guns = 10;
+      bullet_type = bullet1x;
       break;
 
     default:
       guns = 10;
+      bullets = bullet1x;
       gun = BlueGunx1;
       teams = 1;
+      bullet_type = bullet1x;
       eigenteam = Blue;
       break;
     }
@@ -304,20 +319,28 @@ void changeGuns (){
     {
     case 21:
       gun = BlueGunx2;
+      bullets = bullet2x;
+      bullet_type = bullet2x;
       break;
     case 22:
       gun = BlueGunx3;
+      bullets = bullet3x;
+      bullet_type = bullet3x;
       break;
     case 23:
       gun = BlueGunx1;
+      bullets = bullet1x;
+      bullet_type = bullet1x;
       guns = 10;
       break;
 
     default:
       guns = 20;
       gun = RedGunx1;
+      bullets = bullet1x;
       teams = 2;
       eigenteam = Red;
+      bullet_type = bullet1x;
       break;
     }
     break;
@@ -326,18 +349,26 @@ void changeGuns (){
     {
     case 31:
       gun = GreenGunx2;
+      bullets = bullet2x;
+      bullet_type = bullet2x;
       break;
     case 32:
       gun = GreenGunx3;
+      bullets = bullet3x;
+      bullet_type = bullet3x;
       break;
     case 33:
       gun = GreenGunx1;
+      bullets = bullet1x;
+      bullet_type = bullet1x;
       guns = 30;
       break;
 
     default:
       guns = 30;
       gun = GreenGunx1;
+      bullets = bullet1x;
+      bullet_type = bullet1x;
       teams = 3;
       eigenteam = Green;
       break;
@@ -348,75 +379,63 @@ void changeGuns (){
     {
     case 41:
       gun = WhiteGunx2;
+      bullets = bullet2x;
+      bullet_type = bullet2x;
       break;
     case 42:
+      bullets = bullet3x;
       gun = WhiteGunx3;
+      bullet_type = bullet3x;
       break;
     case 43:
+      bullets = bullet1x;
       gun = WhiteGunx1;
       guns = 40;
+      bullet_type = bullet1x;
       break;
 
     default:
+      bullets = bullet1x;
       guns = 40;
       gun = WhiteGunx1;
       teams = 4;
       eigenteam = White;
+      bullet_type = bullet1x;
       break;
     }
     break;
           
   default:
     break;
-  } 
+  }
+  
 }
 
 
 void loop() {
-  while (Health > 0)
+  while (Health > 1) // as long as you have health you are part of the game, when your hp is drained, the gun doesn't read inputs anymore.
   {
-    leds[0] = CRGB (0, 255, 0);
-    FastLED.show();
-    //delay(2000);
-    leds[0] = CRGB (0, 0, 0);
-    FastLED.show();
-    /*switch (Health)
+    if (digitalRead(Reload_Button) == HIGH){
+      bullets = bullet_type;
+
+    }
+    if (Health = 9)
     {
-    case 9:
-    case 8:
-      leds[0] = CRGB (0, 255, 0);
-      break;
-    case 7:
       leds[0] = CRGB (0,255,0);
-      delay(200);
-      leds[0] = CRGB (0, 0, 0);
-      break;
-    case 6:
-    case 5:
+    }
+    if (Health = 6)
+    {
       leds[0] = CRGB (255,255,0);
-      break;     
-    case 4:
-      leds[0] = CRGB (255,255,0);
-      delay(200);
-      leds[0] = CRGB (0, 0, 0);
-      break;
-    case 3:
-    case 2:
+    }
+    if (Health = 3)
+    {
       leds[0] = CRGB (255,0,0);
-      break;
-    case 1:
-      leds[0] = CRGB (255,0,0);
-      delay(200);
-      leds[0] = CRGB (0, 0, 0);                         
-    default:
-      break;
-    }*/
-    if (digitalRead(button_Shoot) == HIGH)
+    }
+    if (digitalRead(button_Shoot) == HIGH) //readout of pin to detect if button is pressed, and will if so run the function "gun" (aka, it fires the "laser")
     {
       prepare_shot(gun);
-      Serial.println("Gun: " + gun);
     }
-    if (digitalRead(ChangeTeams_Button) == HIGH)
+    if (digitalRead(ChangeTeams_Button) == HIGH) //reads the teams pin, if the pin is high, the gun will change team, this is done by running the change teams function
     {
       Serial.println("change team");
       String tijdelijk = ChangeTeams(teams);
@@ -432,16 +451,18 @@ void loop() {
       
       
     }
-    if ((digitalRead(ChangeGuns_Button) == HIGH)and (eigenteam = FFA))
+    if ((digitalRead(ChangeGuns_Button) == HIGH)and (eigenteam = FFA)) //reads the change guns pin, and will attempt to change 'gun type' if it is possible in the given team
     {
       changeGuns();
+           
       Serial.println("Gun: " + gun);
       Serial.println("Gun ID: " + guns);
       Serial.println("Team: " + eigenteam); 
     }    
     
     
-    if (irrecv.decode(&results)) {
+    if (irrecv.decode(&results)) { //this code will try to read/decode the incomming  signals, and will also post them in the serial interface.
+                                   //this will also call the function to take damage (and to recognise the other gun).
    
       uint32_t now = millis();
    
@@ -454,9 +475,8 @@ void loop() {
     }
     delay(200);
   }
-
-  delay(100);
-  Serial.println("u dead m8");
+  delay(200);
+  Serial.println("u dead m8");//this is the message you get when you die
   leds[0] = CRGB (255,0,0);
   delay(100  );
   leds[0] = CRGB (0, 0, 0);
